3544c15dfd1c239e0941065c9e91d821077938db
ChanyutJean <chanyut22@hotmail.com>
2022-03-25 15:22:16 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return "Hello world!"
+
+
+@app.get("/{index}")
+async def get_fibonacci(index: int):
+    previous = 1
+    current = 1
+    for i in range(3, index + 1):
+        current += previous
+        previous = current - previous
+    return str(current)
